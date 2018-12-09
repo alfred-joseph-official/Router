3b394a5e3fbef76368ce914c0547075652804bd2
AjOEM01 <aj.oem.01@gmail.com>
2018-12-09 10:58:47 +0530

    Creating basic settings screen for the router.

---
+++ app/src/main/java/com/example/android/router/WirelessBasicSettings.java
@@ -0,0 +1,13 @@
+package com.example.android.router;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WirelessBasicSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wireless_basic_settings);
+    }
+}
